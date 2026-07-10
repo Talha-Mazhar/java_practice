0f97e21b50a019d8f64038734a67c3b61becac91
Talha-Mazhar <talhamazhar.dev@gmail.com>
2026-07-11 00:09:22 +0500

    feat: withour lambda disbale csrf and other filters

--- SpringSecurity/src/main/java/org/example/springsecurity/config/SecurityConfig.java
+++ SpringSecurity/src/main/java/org/example/springsecurity/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -15,14 +16,23 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http.csrf(customizer -> customizer.disable());
+//        http.csrf(customizer -> customizer.disable());
+//
+//        http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
+//
+//        http.formLogin(Customizer.withDefaults());
+//        http.httpBasic(Customizer.withDefaults());
+//
+//        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
-        http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
+        Customizer<CsrfConfigurer<HttpSecurity>> custCsrf = new Customizer<CsrfConfigurer<HttpSecurity>>() {
+            @Override
+            public void customize(CsrfConfigurer<HttpSecurity> configurer) {
+                configurer.disable();
+            }
+        };
 
-        http.formLogin(Customizer.withDefaults());
-        http.httpBasic(Customizer.withDefaults());
-
-        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.csrf(custCsrf);
 
         return http.build();
     }
